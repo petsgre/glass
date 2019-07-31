18fe163cc469ee6cbeccad7d2dd040afea0280c7
Hunterang <92307127@qq.com>
2019-08-01 00:10:39 +0800

    添加详情页

--- pages/know/index.js
+++ pages/know/index.js
@@ -1,66 +1,26 @@
-// pages/know/index.js
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    sliderInfo: [{
+      image: 'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+    }],
+    info: {
+      name: '张倩',
+      position: '新手咨询师',
+      cert: '国家二级心理咨询师',
+      address: '上海'
+    },
+    detail: {
+      price: '400/次',
+      ways: '视频/当面',
+      person: '1.5k人'
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  handlePathTo(e) {
+   
 
   }
 })
